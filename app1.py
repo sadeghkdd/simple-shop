a67ff6894735ab93ab9fe08539d7a734f0061a51
sadegh <145167212+sadeghkdd@users.noreply.github.com>
2025-01-14 16:28:02 +0330

    app1.py

--- app1.py
+++ app1.py
@@ -4,18 +4,19 @@ import sqlite3
 conn = sqlite3.connect('products.db')
 c = conn.cursor()
 
-c.execute('SELECT name, price, img FROM products')
+c.execute('SELECT name, price, img, link FROM products')  
 rows = c.fetchall()
 
-st.title('Phone Shop:iphone:')
+st.title('Phone Shop 📱')
 
 for i in range(0, len(rows), 3):
     cols = st.columns(3)
     for j, col in enumerate(cols):
         if i + j < len(rows):
-            name, price, img_url = rows[i + j]
+            name, price, img_url, link = rows[i + j]  
             with col:
                 st.image(img_url, caption=name)
                 st.write(f'Price: {price}')
+                st.markdown(f'[View on eBay]({link})')  
 
 conn.close()
