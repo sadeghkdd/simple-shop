61dcc02ea0e95bc3acdfd803e8cb3232dc4e7c40
sadegh <145167212+sadeghkdd@users.noreply.github.com>
2025-01-14 16:17:35 +0330

    app.py

--- app.py
+++ app.py
@@ -6,27 +6,32 @@ response = requests.get("https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432
 soup = BeautifulSoup(response.text, 'html.parser')
 
 products = soup.find_all("div", class_="s-item__info")
-images  = soup.find_all("div", class_="s-item__image-wrapper image-treatment")
+images = soup.find_all("div", class_="s-item__image-wrapper image-treatment")
+links = soup.find_all("a", class_="s-item__link")
 
 conn = sqlite3.connect('products.db')
 c = conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS products
-             (name TEXT, price TEXT, img TEXT)''')
+             (name TEXT, price TEXT, img TEXT, link TEXT)''')
 
+c.execute('''DELETE FROM products''')
+conn.commit()
 
-for product, img in zip(products, images):
+for product, img, link in zip(products, images, links):
     title = product.find("span", role="heading")
     price = product.find("span", class_="s-item__price")
     img_tag = img.find("img")
+    link_tag = link.get('href')
     
-    if title and price and img_tag:
+    if title and price and img_tag and link_tag:
         print(f"name: {title.text}")
         print(f"price: {price.text}")
-        print(f"image: {img_tag['src']}\n")
+        print(f"image: {img_tag['src']}")
+        print(f"link: {link_tag}")
         
-        c.execute("INSERT INTO products (name, price, img) VALUES (?, ?, ?)", (title.text.strip(), price.text.strip(), img_tag['src']))
-
+        c.execute("INSERT INTO products (name, price, img, link) VALUES (?, ?, ?, ?)",
+                  (title.text.strip(), price.text.strip(), img_tag['src'], link_tag))
 
 conn.commit()
 conn.close()
